6106f8e64ce5e9da6f30c15dc5158ca3c23e978b
yoli <yoli@pdx.edu>
2017-12-12 17:34:06 -0800

    admin layout

--- catalog/models.py
+++ catalog/models.py
@@ -50,6 +50,14 @@ class Book(models.Model):
         """
         return reverse('book-detaiil', args=[str(self.id)])
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+
+    display_genre.short_description = 'Genre'
+
 
 
 import uuid # Required for unique book instances
